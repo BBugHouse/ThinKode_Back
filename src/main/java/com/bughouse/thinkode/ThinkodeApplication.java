fa877a0186a15a0ff6494544283735707e64d1d0
MIN <alsrb4171@gmail.com>
2024-08-13 12:09:14 +0900

    setting

---
+++ src/main/java/com/bughouse/thinkode/ThinkodeApplication.java
@@ -0,0 +1,13 @@
+package com.bughouse.thinkode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bughouse/thinkode/ThinkodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bughouse.thinkode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
